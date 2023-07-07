4684c4a634658019ebb1ade6b467a6e2de2daafd
ks-grshkv <kseniia.grshkv@yandex.ru>
2023-07-07 20:27:16 +0300

    Still doesn't work

--- models/models.py
+++ models/models.py
@@ -1,29 +1,9 @@
-# -*- coding: utf-8 -*-
-
-# from odoo import models, fields, api
-
-
-# class /../../../../mnt/extra-addons/contact_form_module(models.Model):
-#     _name = '/../../../../mnt/extraons/contact_form_module'
-#     _description = '/../../../../mnt/extrntact_form_module'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    # _name = 'contact_form_module'
 
     experience = fields.Float()
     department = fields.Char()
@@ -34,8 +14,7 @@ class ResPartner(models.Model):
     # first_name = fields.Char(string='First Name')
     last_name = fields.Char(string='Last Name')
     has_first_name = fields.Boolean(
-        compute='_compute_has_firstname',
-        string="has first name", default=False)
+        compute='_compute_has_firstname')
     # name = fields.Char(
     #     compute='_compute_name', recursive=True, store=True, index=True)
 
@@ -55,14 +34,15 @@ class ResPartner(models.Model):
         else:
             print('NNNNNA')
             self.has_first_name = False
-    
+
     def _compute_name(self):
         self.name = str(str(self.first_name) + ' ' + str(self.last_name))
 
     @api.constrains("department")
     def _check_department(self):
         if not self.department.isalpha():
-            raise ValidationError("The department field can accept only alphabetic characters")
+            raise ValidationError(
+                "The department field can accept only alphabetic characters")
 
     @api.constrains("company_name")
     def _check_company_name(self):
@@ -73,11 +53,10 @@ class ResPartner(models.Model):
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
-    # _name = 'contact_form_module'
 
     @api.constrains("name")
-    # не работает
     def _check_name(self):
         if self.name and not self.name.isalnum():
             print('AAAASDADSDSADAD', self.name)
-            raise ValidationError("The company name field can accept only alphanumeric characters")
+            raise ValidationError(
+                "The company name field can accept only alphanumeric characters")
