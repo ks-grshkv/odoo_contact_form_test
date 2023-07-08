91f2c3167e035011a62e49388dca7485ee4652b0
ks-grshkv <kseniia.grshkv@yandex.ru>
2023-07-08 19:52:52 +0300

    Origin country placement fixed

--- models/models.py
+++ models/models.py
@@ -1,15 +1,5 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
-# from odoo import _, exceptions
-
-
-# class EmptyNamesError(exceptions.ValidationError):
-#     def __init__(self, record, value=None):
-#         value = value or _("No name is set.")
-#         self.record = record
-#         self._value = value
-#         self._name = _("Error(s) with partner %d's name.") % record.id
-#         self.args = (self._name, value)
 
 
 class ResPartner(models.Model):
@@ -33,27 +23,31 @@ class ResPartner(models.Model):
     has_first_name = fields.Boolean(default=False)
 
     name = fields.Char(
-        compute='_compute_name', required=False,
+        compute='_compute_name',
+        required=False,
         precompute=True,
         readonly=False,
         store=True,
-        recursive=True, index=True)
+        recursive=True,
+        index=True)
 
     channel_ids = fields.Many2many(
         relation='mail_channel_library_book_partner')
     meeting_ids = fields.Many2many(relation='meetings_partner')
 
-    # @api.constrains("first_name", "last_name")
-    # def _check_name(self):
-    #     """Ensure at least one name is set."""
-    #     for record in self:
-    #         if all(
-    #             (
-    #                 record.type == "contact" or record.is_company,
-    #                 not (record.first_name or record.last_name),
-    #             )
-    #         ):
-    #             raise EmptyNamesError(record)
+    @api.constrains("first_name", "last_name", "new_company_name")
+    def _check_name(self):
+        for record in self:
+            if record.type == "contact":
+                if (
+                    not record.is_company
+                    and (not (record.first_name or record.last_name))
+                ):
+                    raise ValidationError(
+                        "Please set at least one of the names")
+            elif record.is_company and not record.new_company_name:
+                raise ValidationError(
+                        "Please set at least one of the names")
 
     @api.onchange("experience")
     def _onchange_experience(self):
@@ -70,6 +64,10 @@ class ResPartner(models.Model):
     @api.onchange("first_name", "last_name", "new_company_name")
     def _recompute_name(self):
         for record in self:
+            if not record.first_name:
+                record.first_name = ''
+            if not record.last_name:
+                record.last_name = ''
             if not record.first_name or not record.last_name:
                 record.name = record.new_company_name
             if not record.first_name:
@@ -78,23 +76,24 @@ class ResPartner(models.Model):
             else:
                 record.name = record.first_name + ' ' + record.last_name
 
-    def _compute_name(self):
-        """Костыль чтобы сделать precomputed поле изменяемым."""
-        for record in self:
-            if not record.first_name or not record.last_name:
-                record.name = record.new_company_name
-            if not record.first_name:
-                if record.last_name:
-                    record.name = record.last_name                    
-            else:
-                record.name = record.first_name + ' ' + record.last_name
+    # def _compute_name(self):
+    #     """Костыль чтобы сделать precomputed поле изменяемым."""
+    #     for record in self:
+    #         if not record.first_name or not record.last_name:
+    #             record.name = record.new_company_name
+    #         if not record.first_name:
+    #             if record.last_name:
+    #                 record.name = record.last_name
+    #         else:
+    #             record.name = record.first_name + ' ' + record.last_name
 
     @api.constrains("department")
     def _check_department(self):
         if self.department:
             if not self.department.isalpha():
                 raise ValidationError(
-                    "The department field can accept only alphabetic characters")
+                    "The department field can accept "
+                    + "only alphabetic characters")
 
     @api.constrains("new_company_name")
     def _check_company_name(self):
@@ -102,7 +101,8 @@ class ResPartner(models.Model):
             if self.new_company_name:
                 if not str(self.new_company_name).isalnum():
                     raise ValidationError(
-                        "The company name field can accept only alphanumeric characters")
+                        "The company name field can accept "
+                        + "only alphanumeric characters")
 
     @api.constrains("phone", "email")
     def _not_empty_or_blank(self):
@@ -114,16 +114,3 @@ class ResPartner(models.Model):
         if not phone or not email:
             raise ValidationError(
                 "Please check that both email and phone fields are not blank")
-
-
-# class ResCompany(models.Model):
-#     _inherit = 'res.company'
-#     name = fields.Char(related='partner_id.name', string='Company Name', required=True, store=True, readonly=False)
-
-#     @api.constrains("name")
-#     def _check_name(self):
-#         print('CHECK CONSTRAINT NAME AAAAAAAAA', self.name)
-#         if self.name:
-#             if not str(self.name).isalnum():
-#                 raise ValidationError(
-#                     "The company name field can accept only alphanumeric characters")
